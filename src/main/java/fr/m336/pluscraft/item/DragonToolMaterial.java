705bc44eac86f996b1a37c5a127c90fe90eb1ffb
BuildTools <unconfigured@null.spigotmc.org>
2023-03-20 18:08:45 +0100

    PlusCraft 1.0.1

---
+++ src/main/java/fr/m336/pluscraft/item/DragonToolMaterial.java
@@ -0,0 +1,43 @@
+package fr.m336.pluscraft.item;
+
+import net.minecraft.item.ToolMaterial;
+import net.minecraft.recipe.Ingredient;
+
+public class DragonToolMaterial implements ToolMaterial {
+    private static final int DURABILITY = 16384;
+    private static final float MINING_SPEED_MULTIPLIER = 60.0F;
+    private static final float ATTACK_DAMAGE = 30.0F;
+    private static final int MINING_LEVEL = 4;
+    private static final int ENCHANTABILITY = 15;
+    private static final Ingredient REPAIR_INGREDIENT = Ingredient.ofItems(ModItems.OVERLOADED_MATERIAL);
+
+    @Override
+    public int getDurability() {
+        return DURABILITY;
+    }
+
+    @Override
+    public float getMiningSpeedMultiplier() {
+        return MINING_SPEED_MULTIPLIER;
+    }
+
+    @Override
+    public float getAttackDamage() {
+        return ATTACK_DAMAGE;
+    }
+
+    @Override
+    public int getMiningLevel() {
+        return MINING_LEVEL;
+    }
+
+    @Override
+    public int getEnchantability() {
+        return ENCHANTABILITY;
+    }
+
+    @Override
+    public Ingredient getRepairIngredient() {
+        return REPAIR_INGREDIENT;
+    }
+}
