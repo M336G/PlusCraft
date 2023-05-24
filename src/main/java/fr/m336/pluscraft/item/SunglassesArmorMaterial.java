ed0c64eddb35b88d23230af4a3af6fa2e34e4e00
BuildTools <unconfigured@null.spigotmc.org>
2023-05-24 17:29:11 +0200

    PlusCraft 1.1.5

---
+++ src/main/java/fr/m336/pluscraft/item/SunglassesArmorMaterial.java
@@ -0,0 +1,81 @@
+package fr.m336.pluscraft.item;
+
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.item.ArmorMaterial;
+import net.minecraft.item.Items;
+import net.minecraft.recipe.Ingredient;
+import net.minecraft.sound.SoundEvent;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.util.Lazy;
+
+import java.util.function.Supplier;
+
+public enum SunglassesArmorMaterial implements ArmorMaterial {
+    SUNGLASSES("sunglasses", 2, new int[]{1, 1, 1, 1}, 28,
+            SoundEvents.ITEM_ARMOR_EQUIP_IRON, 0.0f, 0.0f, () -> Ingredient.ofItems(Items.IRON_INGOT));
+
+    private static final int[] BASE_DURABILITY;
+    private final String name;
+    private final int durabilityMultiplier;
+    private final int[] protectionAmounts;
+    private final int enchantability;
+    private final SoundEvent equipSound;
+    private final float toughness;
+    private final float knockbackResistance;
+    private final Lazy<Ingredient> repairIngredientSupplier;
+
+    private SunglassesArmorMaterial(String name, int durabilityMultiplier, int[] protectionAmounts, int enchantability, SoundEvent equipSound, float toughness, float knockbackResistance, Supplier<Ingredient> repairIngredientSupplier) {
+        this.name = name;
+        this.durabilityMultiplier = durabilityMultiplier;
+        this.protectionAmounts = protectionAmounts;
+        this.enchantability = enchantability;
+        this.equipSound = equipSound;
+        this.toughness = toughness;
+        this.knockbackResistance = knockbackResistance;
+        this.repairIngredientSupplier = new Lazy<Ingredient>(repairIngredientSupplier);
+    }
+
+    @Override
+    public int getDurability(EquipmentSlot slot) {
+        return BASE_DURABILITY[slot.getEntitySlotId()] * this.durabilityMultiplier;
+    }
+
+    @Override
+    public int getProtectionAmount(EquipmentSlot slot) {
+        return this.protectionAmounts[slot.getEntitySlotId()];
+    }
+
+    @Override
+    public int getEnchantability() {
+        return this.enchantability;
+    }
+
+    @Override
+    public SoundEvent getEquipSound() {
+        return this.equipSound;
+    }
+
+    @Override
+    public Ingredient getRepairIngredient() {
+        return this.repairIngredientSupplier.get();
+    }
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public float getToughness() {
+        return this.toughness;
+    }
+
+    @Override
+    public float getKnockbackResistance() {
+        return this.knockbackResistance;
+    }
+
+    static {
+        BASE_DURABILITY = new int[]{250, 250, 250, 250};
+    }
+}
